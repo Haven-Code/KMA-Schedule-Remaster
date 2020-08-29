1151ae98bf94257f6b3d7eecf9370a4ee80f666b
Phuc Dang <phuc.px.371@gmail.com>
2020-08-29 10:32:37 +0700

    remove env

--- src/views/Dashboard/About.vue
+++ src/views/Dashboard/About.vue
@@ -1,7 +1,7 @@
 <!-- @format -->
 
 <template>
-	<v-container class="fill-height" fluid="">
+	<v-container class="fill-height" fluid>
 		<v-row align="center" justify="center">
 			<v-col cols="12" sm="10" md="8">
 				<v-card color="amber lighten-3" class="grey--text ">
